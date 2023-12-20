9099c3fe0ec0c939885af012cd36c678f725ce06
ou <706545330@qq.com>
2023-12-21 00:09:48 +0800

    new backbone

--- rd3d/models/backbones_3d/voxformer_backbone.py
+++ rd3d/models/backbones_3d/voxformer_backbone.py
@@ -6,54 +6,164 @@ from ...ops import sfc
 from ...utils.spconv_utils import replace_feature, spconv
 from ...core.base import Register
 
-class Layer(torch.nn.Module):
-    def __init__(self, input_channel, output_channels):
-        super().__init__()
-        self.mlp1 = build_mlps([output_channels], input_channel)
-        self.mlp2 = build_mlps([output_channels], output_channels)
+VFL = Register('VoxFormerLayer')
 
-    def forward(self, x):
-        n, g, c = x.size()
-        loc = self.mlp1(x.view(-1, c)).view(n, g, -1)
-        n, g, c = loc.size()
-        glb = loc.max(dim=1)[0][:, None, :]
-        x = self.mlp2((loc + glb).view(-1, c)).view(n, g, -1)
+
+@VFL.register('ResPointMLP')
+class ResidualPointMLP(torch.nn.Module):
+    def __init__(self, model_cfg, input_channel, group_size):
+        super().__init__()
+        self.norm = self.build_group_norm(input_channel)
+        self.pre_blocks = self.build_res_mlps(self.norm.output_channel, model_cfg.pre)
+        self.pos_blocks = self.build_res_mlps(2 * self.pre_blocks[-1].output_channel, model_cfg.pos)
+        self.agg = lambda x: x.max(dim=1, keepdim=True)[0]
+
+        self.group_size = group_size
+        self.output_channel = model_cfg.pos[-1]
+
+    @staticmethod
+    def build_group_norm(input_channel):
+        class GeoAffine(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.output_channel = 3 + input_channel
+                self.alpha = torch.nn.Parameter(torch.ones([1, 1, self.output_channel]))
+                self.beta = torch.nn.Parameter(torch.zeros([1, 1, self.output_channel]))
+
+            def forward(self, p, x):  # (m,g,c)
+                x = torch.cat((p, x), dim=-1)
+                std, mean = torch.std_mean(x, dim=1, keepdim=True)
+                x = (x - mean) / (std + 1e-5)
+                x = self.alpha * x + self.beta
+                return x
+
+        return GeoAffine()
+
+    @staticmethod
+    def build_res_mlp(input_channel, output_channel):
+        class ResMLP(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.act = torch.nn.ReLU()
+                self.net1 = torch.nn.Sequential(
+                    torch.nn.Linear(input_channel, output_channel, bias=False),
+                    torch.nn.BatchNorm1d(output_channel, eps=1e-3, momentum=0.01),
+                    self.act,
+                )
+                self.net2 = torch.nn.Sequential(
+                    torch.nn.Linear(output_channel, output_channel, bias=False),
+                    torch.nn.BatchNorm1d(output_channel, eps=1e-3, momentum=0.01)
+                )
+
+                self.identify = torch.nn.Sequential(
+                    torch.nn.Linear(input_channel, output_channel, bias=False),
+                    torch.nn.BatchNorm1d(output_channel, eps=1e-3, momentum=0.01)
+                ) if input_channel != output_channel else torch.nn.Identity()
+                self.output_channel = output_channel
+
+            def forward(self, x):  # x(b,c)
+                x = self.act(self.net2(self.net1(x)) + self.identify(x))
+                return x
+
+        return ResMLP()
+
+    @staticmethod
+    def build_res_mlps(input_channel, mlps_cfg):
+        mlps = [input_channel] + mlps_cfg
+        return torch.nn.Sequential(*[
+            ResidualPointMLP.build_res_mlp(mlps[i - 1], mlps[i])
+            for i in range(1, len(mlps))
+        ])
+
+    def group(self, x):
+        return x.view(-1, self.group_size, x.size(-1))
+
+    def flatten(self, x):
+        return x.view(-1, x.size(-1))
+
+    def forward(self, p, x):  # (n*g,3) (n*g,c)
+        x = self.flatten(self.norm(self.group(p), self.group(x)))
+        x = self.group(self.pre_blocks(x))
+        agg = self.agg(x)
+        x = torch.cat((x, agg.expand(-1, x.size(1), -1)), dim=-1)
+        x = self.pos_blocks(self.flatten(x))
         return x
 
 
-class Block(torch.nn.Module):
-    def __init__(self, cfg, input_channel, group_size):
+@VFL.register('PointMLP')
+class PointNet(torch.nn.Module):
+    def __init__(self, model_cfg, input_channel, group_size):
         super().__init__()
+        self.norm = self.build_pos_embed(input_channel)
+        self.pre_blocks = self.build_mlps(self.norm.output_channel, model_cfg.pre)
+        self.pos_blocks = self.build_mlps(2 * model_cfg.pre[-1], model_cfg.pos)
+        self.agg = lambda x: x.max(dim=1, keepdim=True)[0]
+
         self.group_size = group_size
-        self.premap = build_mlps(cfg[:1], input_channel)
-        self.layers = torch.nn.Sequential(
-            *[Layer(cfg[i], cfg[i + 1])
-              for i in range(0, len(cfg) - 1)]
+        self.output_channel = model_cfg.pos[-1]
+
+    @staticmethod
+    def build_pos_embed(input_channel):
+        class AddXYZ(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.output_channel = 3 + input_channel
+
+            def forward(self, p, x):  # (m,g,c)
+                x = torch.cat((p - p.mean(dim=1, keepdim=True), x), dim=-1)
+                return x
+
+        return AddXYZ()
+
+    @staticmethod
+    def build_mlp(input_channel, output_channel):
+        return torch.nn.Sequential(
+            torch.nn.Linear(input_channel, output_channel, bias=False),
+            torch.nn.BatchNorm1d(output_channel),
+            torch.nn.ReLU()
         )
-        self.enpos = build_mlps(cfg[:1], 3)
+
+    @staticmethod
+    def build_mlps(input_channel, mlps_cfg):
+        mlps_cfg = [input_channel] + mlps_cfg
+        return torch.nn.Sequential(*[
+            PointNet.build_mlp(mlps_cfg[i - 1], mlps_cfg[i])
+            for i in range(1, len(mlps_cfg))
+        ])
 
     def group(self, x):
-        return x.view(-1, self.group_size, x.shape[-1])
+        return x.view(-1, self.group_size, x.size(-1))
 
     def flatten(self, x):
-        return x.view(-1, x.shape[-1])
+        return x.view(-1, x.size(-1))
 
-    def forward(self, p, x, i_to, i_from):
-        # x = self.premap(x)
-        #
-        # p1 = p
-        # e1 = self.group(p1)
-        # e1 = self.enpos(self.flatten(e1 - e1.mean(dim=1, keepdim=True)))
-        #
-        # p2 = p1[i_to]
-        # e2 = self.group(p2)
-        # e2 = self.enpos(self.flatten(e2 - e2.mean(dim=1, keepdim=True)))
-        #
-        # x1 = x
-        # x1 = x1 + self.flatten(self.layers(self.group(e1 * x1)))  # (kg,c) -> (k,g,c)
-        # x2 = x1[i_to]
-        # x2 = x2 + self.flatten(self.layers(self.group(e2 * x2)))
+    def forward(self, p, x):
+        x = self.flatten(self.norm(self.group(p), self.group(x)))
+        x = self.group(self.pre_blocks(x))
+        agg = self.agg(x)
+        x = torch.cat((x, agg.expand(-1, x.size(1), -1)), dim=-1)
+        x = self.pos_blocks(self.flatten(x))
+        return x
 
+
+@VFL.register('Attention')
+class Attention():
+    pass
+
+
+class MultiScaleLayer(torch.nn.Module):
+    def __init__(self, network, strides=(4, 16)):
+        super().__init__()
+        assert len(strides) > 0
+
+        self.network = network
+        self.strides = strides
+        self.agg = build_mlps([self.network.output_channel],
+                              (1 + len(strides)) * self.network.output_channel)
+        self.group_size = self.network.group_size
+        self.output_channel = self.network.output_channel
+
+    def backup_forward(self):
         x = self.premap(x)
 
         p1 = p
@@ -68,20 +178,49 @@ class Block(torch.nn.Module):
         x2 = self.flatten(self.layers(self.group(e2 * x1[i_to])))
         return p2, x2, i_from, i_to
 
+    def forward(self, p, x):  # (n*g,3) (n*g,c) [(n*g),...,(n*g)]
+        feats = [self.network(p, x)]
+        for stride in self.strides:
+            assert self.group_size % stride == 0
+            self.network.group_size = self.group_size // stride
+            feats.append(self.network(p, x))
+            self.network.group_size = self.group_size
+
+        return self.agg(torch.cat(feats, dim=-1))
+
+
+def highlight_group(pts, group_id, gs):
+    import torch
+    from rd3d.utils import viz_utils
+    from matplotlib import pyplot as plt
+    if group_id:
+        c = torch.zeros_like(pts).view(-1, gs, 3).cpu().float()
+        gc = torch.randperm(len(group_id))[:, None].repeat(1, gs).view(-1)
+        gc = torch.tensor(plt.get_cmap('tab20c')(gc / gc.max())[:, :3]).float()
+        c[group_id] = gc.view(-1, gs, 3)
+        viz_utils.viz_scene((pts, c.view(-1, 3)))
+    else:
+        viz_utils.viz_scene(pts)
+
 
 class CurveBackBone(torch.nn.Module):
     def __init__(self, model_cfg, input_channels, grid_size, **kwargs):
         super().__init__()
         self.cfg = model_cfg
         self.group_size = self.cfg.GROUP_SIZE
-        self.grid_size = torch.tensor(grid_size, dtype=torch.int).view(1, 3).cuda()
-        mlps = self.cfg.BLOCKS
 
         self.blocks = torch.nn.ModuleList()
-        for mlp in mlps:
-            self.blocks.append(Block(mlp, input_channels, self.group_size))
-            input_channels = mlp[-1]
+
+        for layer_cfg in self.cfg.BLOCKS:
+            layer = VFL.build_from_cfg(layer_cfg,
+                                       input_channel=input_channels,
+                                       group_size=self.group_size)
+            if self.cfg.get('MULTI_SCALE_STRIDES', ()):
+                layer = MultiScaleLayer(layer, self.cfg.MULTI_SCALE_STRIDES)
+            self.blocks.append(layer)
+            input_channels = layer.output_channel
         self.num_point_features = input_channels
+        self.grid_size = torch.tensor(grid_size, dtype=torch.int).view(1, 3).cuda()
         self.order = sfc.min_required_order(self.grid_size)
 
     def mapping(self, vox_numbs, vox_coors):
@@ -107,14 +246,15 @@ class CurveBackBone(torch.nn.Module):
         vox_feats = batch_dict['voxel_features']
         pts_coors = batch_dict['point_coords']
 
-        (ind1, ind2, ind12, ind21), vox_numbs = self.mapping(vox_numbs, vox_coors)
+        (ind1, _, ind12, ind21), vox_numbs = self.mapping(vox_numbs, vox_coors)
+        ind_list = [ind1] + [ind12, ind21] * (len(self.blocks) // 2)
 
-        pts_coors = pts_coors[ind1]
-        vox_coors = vox_coors[ind1]
-        vox_feats = vox_feats[ind1]
-
-        for i, block in enumerate(self.blocks):
-            pts_coors, vox_feats, ind12, ind21 = block(pts_coors, vox_feats, ind12, ind21)
+        for ind, block in zip(ind_list, self.blocks):
+            vox_feats = vox_feats[ind]
+            pts_coors = pts_coors[ind]
+            vox_coors = vox_coors[ind]
+            vox_feats = block(pts_coors, vox_feats)
+            # highlight_group(pts_coors.split(vox_numbs.tolist(), dim=0)[3], [0], self.group_size)
 
         batch_dict.update({
             'voxel_numbers': vox_numbs,
@@ -122,5 +262,4 @@ class CurveBackBone(torch.nn.Module):
             'voxel_features': vox_feats,
             'point_coords': pts_coors,
         })
-        # highlight_group(pts_coors.split(vox_numbs.tolist(), dim=0)[3], [0], self.group_size)
         return batch_dict
